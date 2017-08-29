fd93f2a82fdb28649003664ef9c37a0096f29b39
Simone Gentili <sensorario@gmail.com>
2017-08-29 21:50:17 +0200

    newspaper metaphore

--- main.go
+++ main.go
@@ -8,6 +8,26 @@ import (
 
 var addr = flag.String("addr", ":8080", "http service address")
 
+func main() {
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/config.js", serveConfig)
+	http.HandleFunc("/game.js", serveJs)
+	http.HandleFunc("/game.css", serveCss)
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
 	if r.URL.Path != "/" {
@@ -31,24 +51,4 @@ func serveCss(w http.ResponseWriter, r *http.Request) {
 
 func serveConfig(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "client/config.js")
-}
-
-func main() {
-	flag.Parse()
-	hub := newHub()
-	go hub.run()
-
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/config.js", serveConfig)
-	http.HandleFunc("/game.js", serveJs)
-	http.HandleFunc("/game.css", serveCss)
-
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(hub, w, r)
-	})
-
-	err := http.ListenAndServe(*addr, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
 }
